95a4b12f91a806fea8171c6294682c25d9e9d6e0
Dandhara Ribeiro <dandhararibeiro@hotmail.com>
2019-05-09 11:11:05 -0300

    Alteraçã no arquivo princial da aplicaçao

--- src/main/java/com/testespring/projetospring/MeuPrimeiroProjetoSpringApplication.java
+++ src/main/java/com/testespring/projetospring/MeuPrimeiroProjetoSpringApplication.java
@@ -8,6 +8,6 @@ public class MeuPrimeiroProjetoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoSpringApplication.class, args);
+		System.out.println("Meu primeiro projeto");
 	}
-
 }
